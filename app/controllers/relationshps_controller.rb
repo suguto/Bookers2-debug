c23095073d1fa56a809be535b41ac063b12c9b69
EC2 Default User <ec2-user@ip-172-31-1-98.ec2.internal>
2022-06-01 13:53:34 +0000

    フォロー機能

--- app/controllers/relationshps_controller.rb
+++ app/controllers/relationshps_controller.rb
@@ -1,21 +1,31 @@
 class RelationshpsController < ApplicationController
+
   def following
+    @user=User.find(params[:user_id])
+    @users=@user.followings.page(params[:page]).per(5)
   end
-  
+
   def follower
+    @user=User.find(params[:user_id])
+    @users=@user.follwers
   end
-  
+
   def create
+    @user=User.find(params[:follower_id])
+    current_user.follow(@user)
+    redirect_to request.referer
   end
-  
+
   def destroy
+    @user=User.find(params[:follower_id])
+    current_user.unfollow(@user)
   end
-  
-  
+
+
   private
-  
+
   def relationship_params
     params.require(:relationship).permit(:follower, :follewd)
   end
-  
+
 end
